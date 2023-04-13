8b2b9c50532adc8fa84c32b0c3dfb2509f669d7b
AurelieGnt <99643639+AurelieGnt@users.noreply.github.com>
2023-04-13 15:33:58 +0200

    Sol recursive 1er jet
    (vous spoilez pas s ivous voulez pas lire) ;)

--- Plante-carnivore/Solrecurs.java
+++ Plante-carnivore/Solrecurs.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 import java.io.*; 
 
 // Sol récursive 
-// force brute: l'idée est de trouver une fleur envahissante dans le tableau 
+// force brute: l'idée est de trouver une fleur envahissante dans le tableau grâce à la méthode diviser pour régner
 
 public class Solrecurs {
-    public static void main(String args[])
+  public static String main(String args[])
   {
     try
     {
@@ -26,9 +26,9 @@ public class Solrecurs {
         br.readLine(); 
         String m_plantes = br.readLine(); 
         // Crée le tableau contenant les plantes
-        String[]plantes = m_plantes.split(", "); 
-        trouver_envahisseur(plantes);
-        i+=1; 
+        String[]plantes = m_plantes.split(", ");
+        i+=1;  
+        return(trouver_envahisseur(plantes, 0, plantes.length)); 
       }
 
       fr.close();
@@ -40,34 +40,42 @@ public class Solrecurs {
     }
   }
 
-    public static void trouver_envahisseur(String[] fleurs)
-    {  
-        ArrayList<String> fleurEnvahissante = new ArrayList<>();
+
+
+  public static String trouver_envahisseur(String[] fleurs, int debut, int fin)
+  { 
+    // Cas de base : 1 seule fleur dans le tableau  
+    if (fleurs.length == 1){
+      return fleurs[0]; 
+    }
+
+    // Sinon 
+    String partieGauche = trouver_envahisseur(String[] fleurs, 0, fleurs.length/2 )
+    String partieDroite = trouver_envahisseur(String[] fleurs, fleurs.length/2, fleur.length) 
+    // Dans le cas d'un tab contenant un nombre impair de fleurs, partieDroite prendra en compte un élément supplémentaire que partieGauche
+
     
-        for (String fleur : fleurs){
-            int compteur = compterOccurence(fleurs, fleur);
-            if ( compteur > fleurs.length/2){
-                if (!fleurEnvahissante.contains(fleur)){
-                    fleurEnvahissante.add(fleur); 
-                }
-            }
-        }
-
-        if (fleurEnvahissante.size() == 0) {
-            System.out.println("null");
-        }
-        else{
-            System.out.println(fleurEnvahissante.get(0));
-        }  
+    // Comparer les résultats 
+    if (partieGauche = partieDroite) {
+        return partieGauche;
     }
+    else{
+      return partieDroite; // Car partieDroite prend en compte un élement en plus que PartieGauche
+    } 
 
-    public static int compterOccurence (String[] tab, String elem){
-        int compteur = 0; 
-        for (int i=0; i< tab.length; i++){
-            if (tab[i].equals(elem)){
-                compteur++;
-            }
-        }
-        return(compteur); 
-    }
+    int EnvahisseurGauche = compterOccurence(partieGauche[], partieGauche )
+    int EnvahisseurDroite = compterOccurence(partie[], partieDroite)
+    
+  }
+
+
+  public static int compterOccurence (String[] tab, String elem){
+      int compteur = 0; 
+      for (int i=0; i< tab.length; i++){
+          if (tab[i].equals(elem)){
+              compteur++;
+          }
+      }
+      return(compteur); 
+  }
 }
